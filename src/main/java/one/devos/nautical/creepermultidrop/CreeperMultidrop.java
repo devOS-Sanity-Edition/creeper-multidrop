522a04268f7c177354aaa0cbe771fdab2a85e9ba
Storm Anderson <storm@artrinix.xyz>
2022-07-19 23:48:02 +1200

    Fix mod working client-side as well as server-side

--- src/main/java/one/devos/nautical/creepermultidrop/CreeperMultidrop.java
+++ src/main/java/one/devos/nautical/creepermultidrop/CreeperMultidrop.java
@@ -1,11 +1,11 @@
 package one.devos.nautical.creepermultidrop;
 
-import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class CreeperMultidropServer implements DedicatedServerModInitializer {
+public class CreeperMultidrop implements ModInitializer {
     public static final String MOD_ID = "creepermultidrop";
     public static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().getFriendlyString();
     public static final String MOD_NAME = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getName();
@@ -13,7 +13,7 @@ public class CreeperMultidropServer implements DedicatedServerModInitializer {
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
     @Override
-    public void onInitializeServer() {
+    public void onInitialize() {
         LOGGER.info("[" + MOD_NAME + "] Version " + VERSION + " loaded." );
         LOGGER.info("[" + MOD_NAME + "] " + "Want head?");
     }
