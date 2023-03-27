22e1a898925057953a19b8c1baef4fbdc9c97576
solonovamax <solonovamax@12oclockpoint.com>
2023-03-26 21:31:06 -0400

    Update to 1.19.2

--- src/main/java/one/devos/nautical/creepermultidrop/mixins/CreeperMixin.java
+++ src/main/java/one/devos/nautical/creepermultidrop/mixins/CreeperMixin.java
@@ -1,5 +1,6 @@
 package one.devos.nautical.creepermultidrop.mixins;
 
+
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Creeper;
@@ -8,20 +9,22 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
+
 @Mixin(Creeper.class)
 public abstract class CreeperMixin extends Entity {
     public CreeperMixin(EntityType<?> entityType, Level level) {
         super(entityType, level);
     }
-
-    @Shadow public abstract boolean isPowered();
-
+    
     /**
      * @author IMS
      * @reason Literally the whole mod.
      */
     @Overwrite
     public boolean canDropMobsSkull() {
-        return this.isPowered() || this.random.nextInt(10) == 1;
+        return this.isPowered();
     }
+    
+    @Shadow
+    public abstract boolean isPowered();
 }
